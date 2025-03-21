c56a5118e5b00c8c9dd3c310ae72bd5c7fea478a
Andrian Moskalenko <andrianmoskalenko@gmail.com>
2025-03-21 18:22:37 +0200

    Initial commit: project setup

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(glb|gltf)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next/static/',
+          outputPath: 'static/',
+        },
+      },
+    });
+    return config;
+  },
+};
+
+module.exports = nextConfig; 
